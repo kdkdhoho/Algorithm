9f862aa2097aba79aee2692a977cbcbb5298c73c
DongHo Kim <hkim4410@naver.com>
2022-08-14 12:11:52 +0900

    예산 추가

---
+++ 그리디/예산.java
@@ -0,0 +1,21 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12982?language=java
+
+import java.util.*;
+
+class Solution {
+  public int solution(int[] d, int budget) {
+    int answer = 0;
+
+    Arrays.sort(d);
+
+    for (int i = 0; i < d.length; i++) {
+      if (d[i] <= budget) {
+        answer++;
+        budget -= d[i];
+      }
+    }
+
+    return answer;
+  }
+}
