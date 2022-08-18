8d6c177fc250005fffd1adf0a00d2963966b3d74
DongHo Kim <hkim4410@naver.com>
2022-08-18 21:18:08 +0900

    사칙연산 추가

---
+++ 구현/수학/사칙연산.java
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/10869
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+    System.out.println(A + B);
+    System.out.println(A - B);
+    System.out.println(A * B);
+    System.out.println(A / B);
+    System.out.println(A % B);
+  }
+}
