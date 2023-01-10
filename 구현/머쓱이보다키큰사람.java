029ee7764036a2df2d06b8d45f48c1219b9685c4
DongHo Kim <hkim4410@naver.com>
2023-01-10 15:23:05 +0900

    머쓱이보다 키 큰 사람

--- 구현/머쓱이보다키큰사람.java
+++ 구현/머쓱이보다키큰사람.java
@@ -1,8 +1,6 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
-public class n의배수고르기 {
-
+class Solution {
     public int solution(int[] array, int height) {
         long answer = Arrays.stream(array)
                 .filter(num -> num > height)
