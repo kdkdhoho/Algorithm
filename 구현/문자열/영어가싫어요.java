05dca2132393a559cfb64719dd4d0356f6fb92ac
DongHo Kim <hkim4410@naver.com>
2023-01-14 13:58:22 +0900

    refactor(영어가 싫어요): 코드 개선

--- 구현/문자열/영어가싫어요.java
+++ 구현/문자열/영어가싫어요.java
@@ -1,22 +1,10 @@
-import java.util.Map;
-
 class Solution {
-    Map<String, Integer> words = Map.of("zero", 0, "one", 1, "two", 2, "three", 3, "four", 4, "five", 5,
-            "six", 6, "seven", 7, "eight", 8, "nine", 9);
-    StringBuilder answer = new StringBuilder();
+    String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
     public long solution(String numbers) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < numbers.length(); i++) {
-            String substring = numbers.substring(i, i + 1);
-            stringBuilder.append(substring);
-
-            if (words.containsKey(stringBuilder.toString())) {
-                answer.append(words.get(stringBuilder.toString()));
-                stringBuilder.setLength(0);
-            }
+        for (int i = 0; i < words.length; i++) {
+            numbers = numbers.replaceAll(words[i], String.valueOf(i));
         }
-
-        return Long.parseLong(answer.toString());
+        return Long.parseLong(numbers);
     }
 }
