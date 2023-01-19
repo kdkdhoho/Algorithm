793da10c5e115ec8ae086c4d98bcaf465fc0ac18
DongHo Kim <hkim4410@naver.com>
2023-01-19 09:49:57 +0900

    치킨 쿠폰

--- 구현/수학/치킨쿠폰.java
+++ 구현/수학/치킨쿠폰.java
@@ -1,2 +1,13 @@
-package 수학;public class 치킨쿠폰 {
+class Solution {
+    int answer = 0;
+
+    public int solution(int chicken) {
+        int coupon = chicken;
+
+        while (coupon >= 10) {
+            answer += coupon / 10;
+            coupon = (coupon % 10) + (coupon / 10);
+        }
+        return answer;
+    }
 }
